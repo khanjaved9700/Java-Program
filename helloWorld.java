965598f362782d163f269c8e3257b8dc8f435bf6
Javed <87005546+khanjaved9700@users.noreply.github.com>
2021-07-06 18:05:15 +0530

    HelloWorld Java Program

---
+++ helloWorld.java
@@ -0,0 +1,8 @@
+class hellWorld{
+  public static void main(string args[]){
+    System.out.println("Hello World");
+    System.out.println("How are you");
+    System.out.println("This is my first program on github and I am so excited!");
+  }
+}
+// really this is awesome i am curious to learn more and more...
